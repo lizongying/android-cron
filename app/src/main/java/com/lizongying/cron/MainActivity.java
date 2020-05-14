2b884dd9e71fa9cac2bffdbdcc458bd6b77a09db
lizongying <lizongying@msn.com>
2020-05-14 11:17:18 +0800

    init

---
+++ app/src/main/java/com/lizongying/cron/MainActivity.java
@@ -0,0 +1,158 @@
+package com.lizongying.cron;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int MSG_JOB_START = 0;
+    public static final int MSG_JOB_STOP = 1;
+    public static final int MSG_JOB_SUCCESS = 2;
+    public static final int MSG_JOB_ERROR = 3;
+    private static final String TAG = "MainActivity";
+    public static final String MESSENGER_INTENT_KEY = TAG + ".MESSENGER_INTENT_KEY";
+    public static String WIFI = "";
+    ComponentName mServiceComponent;
+    private int mJobId = 0;
+    private IncomingMessageHandler mHandler;
+    private ProgressBar progress;
+    private FloatingActionButton fab;
+    private boolean running = false;
+    private long START_SERVICE_INTERVAL = 15 * 60 * 1000L;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHandler = new IncomingMessageHandler(this);
+        mServiceComponent = new ComponentName(this, MyJobService.class);
+
+        progress = (ProgressBar) findViewById(R.id.progress);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        progress.setMax((int) START_SERVICE_INTERVAL);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                running = !running;
+                if (running) {
+                    fab.setImageResource(android.R.drawable.button_onoff_indicator_on);
+                    scheduleJob();
+                } else {
+                    fab.setImageResource(android.R.drawable.button_onoff_indicator_off);
+                    cancelJob();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent startServiceIntent = new Intent(this, MyJobService.class);
+        Messenger messengerIncoming = new Messenger(mHandler);
+        startServiceIntent.putExtra(MESSENGER_INTENT_KEY, messengerIncoming);
+        startService(startServiceIntent);
+    }
+
+    @Override
+    protected void onStop() {
+        stopService(new Intent(this, MyJobService.class));
+        super.onStop();
+    }
+
+    public void scheduleJob() {
+        JobInfo.Builder builder = new JobInfo.Builder(mJobId, mServiceComponent);
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED);
+        builder.setPeriodic(START_SERVICE_INTERVAL);
+        JobScheduler mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        assert mJobScheduler != null;
+        mJobScheduler.schedule(builder.build());
+        Toast.makeText(MainActivity.this, R.string.job_scheduled, Toast.LENGTH_LONG).show();
+    }
+
+    public void cancelJob() {
+        JobScheduler mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        assert mJobScheduler != null;
+        mJobScheduler.cancel(mJobId);
+        Toast.makeText(MainActivity.this, R.string.job_canceled, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * {@link Handler}允许您发送与线程相关联的消息。
+     * {@link Messenger}使用此处理程序从{@link MyJobService}进行通信。
+     * 它也用于使开始和停止视图在短时间内闪烁。
+     */
+    private static class IncomingMessageHandler extends Handler {
+
+        private WeakReference<MainActivity> mActivity;
+
+        IncomingMessageHandler(MainActivity activity) {
+            super();
+            this.mActivity = new WeakReference<>(activity);
+        }
+
+
+        @Override
+        public void handleMessage(Message msg) {
+            final MainActivity mSchedulerActivity = mActivity.get();
+            if (mSchedulerActivity == null) {
+                return;
+            }
+            switch (msg.what) {
+                case MSG_JOB_SUCCESS:
+                    Log.e(TAG, mSchedulerActivity.getString(R.string.job_success));
+                    Toast.makeText(mSchedulerActivity, R.string.job_success, Toast.LENGTH_LONG).show();
+                    break;
+                case MSG_JOB_ERROR:
+                    Log.e(TAG, mSchedulerActivity.getString(R.string.job_error));
+                    Toast.makeText(mSchedulerActivity, R.string.job_error, Toast.LENGTH_LONG).show();
+                    break;
+                case MSG_JOB_START:
+                    Log.i(TAG, mSchedulerActivity.getString(R.string.job_started));
+                    Toast.makeText(mSchedulerActivity, R.string.job_started, Toast.LENGTH_LONG).show();
+                    break;
+                case MSG_JOB_STOP:
+                    Log.i(TAG, mSchedulerActivity.getString(R.string.job_finished));
+                    Toast.makeText(mSchedulerActivity, R.string.job_finished, Toast.LENGTH_LONG).show();
+                    CountDownTimer timer = new CountDownTimer(mSchedulerActivity.START_SERVICE_INTERVAL, 60 * 1000L) {
+                        @Override
+                        public void onTick(long l) {
+                            Log.i(TAG, String.valueOf(l));
+                            mSchedulerActivity.progress.setProgress((int) (mSchedulerActivity.START_SERVICE_INTERVAL - l));
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            mSchedulerActivity.progress.setProgress(0);
+                        }
+                    };
+                    timer.start();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/lizongying/cron/MyJobService.java
@@ -0,0 +1,110 @@
+package com.lizongying.cron;
+
+import android.annotation.SuppressLint;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.util.Calendar;
+
+import static com.lizongying.cron.MainActivity.MESSENGER_INTENT_KEY;
+import static com.lizongying.cron.MainActivity.MSG_JOB_ERROR;
+import static com.lizongying.cron.MainActivity.MSG_JOB_START;
+import static com.lizongying.cron.MainActivity.MSG_JOB_STOP;
+import static com.lizongying.cron.MainActivity.MSG_JOB_SUCCESS;
+
+public class MyJobService extends JobService {
+    private static final String TAG = MyJobService.class.getSimpleName();
+
+    private Messenger mActivityMessenger;
+
+    private JobParameters mJobParameters;
+
+    private static boolean isBelong() {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        return (hour == 10)
+                ||
+                (19 < hour);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mActivityMessenger = intent.getParcelableExtra(MESSENGER_INTENT_KEY);
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public boolean onStartJob(final JobParameters params) {
+        mJobParameters = params;
+        @SuppressLint("StaticFieldLeak")
+        AsyncTask<Void, Void, Void> mTask = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                sendMessage(MSG_JOB_START, mJobParameters.getJobId());
+                if (!isBelong()) {
+                    return null;
+                }
+                try {
+                    Intent intent = new Intent();
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setClassName("com.client.xrxs.com.xrxsapp", "com.client.xrxs.com.xrxsapp.activity.WelcomeActivity");
+                    startActivity(intent);
+                    sendMessage(MSG_JOB_SUCCESS, mJobParameters.getJobId());
+                } catch (ActivityNotFoundException e) {
+                    sendMessage(MSG_JOB_ERROR, mJobParameters.getJobId());
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                jobFinished(mJobParameters, true);
+                sendMessage(MSG_JOB_STOP, mJobParameters.getJobId());
+                super.onPostExecute(result);
+            }
+        };
+        mTask.execute();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        sendMessage(MSG_JOB_STOP, params.getJobId());
+        return false;
+    }
+
+    private void sendMessage(int messageID, @Nullable Object params) {
+        if (mActivityMessenger == null) {
+            Log.i(TAG, getString(R.string.from_self));
+            return;
+        }
+        Message m = Message.obtain();
+        m.what = messageID;
+        m.obj = params;
+        try {
+            mActivityMessenger.send(m);
+        } catch (RemoteException e) {
+            Log.e(TAG, getString(R.string.server_error));
+        }
+    }
+}
